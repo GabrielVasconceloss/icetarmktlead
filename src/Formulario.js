35b137201da079e1d4bbe0a0828f4911892c4540
desenvolvimento <gabriel.rvs0@hotmail.com>
2023-11-16 15:52:36 -0300

    Adiciona logotipo e ícone do WhatsApp

---
+++ src/Formulario.js
@@ -0,0 +1,97 @@
+// Formulario.js
+import React, { useState } from 'react';
+import './Formulario.css'; // Importe o arquivo CSS
+
+const Formulario = () => {
+  const [formData, setFormData] = useState({
+    nomeEmpresa: '',
+    responsavelCompras: '',
+    responsavelManutencao: '',
+    email: '',
+    telefone: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode enviar os dados para onde quiser, como uma API ou um servidor.
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <a href='https://api.whatsapp.com/send?phone=5511974636540'>
+        <img
+          src="wpp.png"
+          alt="WhatsApp"
+          className="whatsapp-icon"
+        />
+      </a>
+      <form onSubmit={handleSubmit}>
+        <div className="logo-container">
+          <a href='https://www.icetar.com.br/'>
+            <img src="/logo192.png" alt="Logo" className="logo" hs />
+          </a>
+        </div>
+        <h1>Solicite sua cotação</h1>
+        <label>
+          Nome da Empresa*
+          <input
+            type="text" 
+            name="nomeEmpresa"
+            value={formData.nomeEmpresa}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Responsável por Compras
+          <input
+            type="text"
+            name="responsavelCompras"
+            value={formData.responsavelCompras}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Responsável por Manutenção
+          <input
+            type="text"
+            name="responsavelManutencao"
+            value={formData.responsavelManutencao}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Email*
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Telefone/WhatsApp*
+          <input
+            type="tel"
+            name="telefone"
+            value={formData.telefone}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
